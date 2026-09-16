df7a669d995004342cd92cf0d61c5f2b8615a691
Ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-09-16 14:54:05 +0900

    change!: 問題データをjsonからcsvに変更 (#24)

--- questions/question_loader.gd
+++ questions/question_loader.gd
@@ -8,57 +8,75 @@ var _questions: Array[QuestionData] = []
 func _init(init_path: String) -> void:
 	if not init_path.begins_with("res://"):
 		push_error("問題文には \"res://\" から始まるパスを指定してください")
-	else:
-		_path = init_path
+		return
+
+	_path = init_path
 	_load_file()
 
 
 func _load_file() -> void:
 	_questions.clear()
 
+	if not FileAccess.file_exists(_path):
+		push_warning("問題文ファイル %s が存在しないため、読み込みをスキップします" % _path)
+		return
+
 	var file := FileAccess.open(_path, FileAccess.READ)
 
 	if file == null:
-		push_error("問題文ファイルを開けませんでした: %s" % _path)
-		return
-
-	var json_text := file.get_as_text()
-	var parsed: Variant = JSON.parse_string(json_text)
-
-	if parsed == null:
-		push_error("JSONの解析に失敗しました: %s" % _path)
-		return
-
-	if not parsed is Array:
-		push_error("JSONのルートは配列である必要があります")
-		return
-	
-	for item: Variant in parsed:
-		if not item is Dictionary:
-			push_warning("Dictionaryではないデータを無視しました")
-			continue
-
-		var data: Dictionary = item
-
-		if not data.has("question") or not data.has("difficulty"):
-			push_warning("必要な項目がないデータを無視しました")
-			continue
-
-		if not data["question"] is String:
-			push_warning("textがStringではないデータを無視しました")
-			continue
-
-		# String.to_float() を利用してパースしているらしいのでflaotで受ける
-		if not data["difficulty"] is float:
-			push_warning("difficultyが数値ではないデータを無視しました")
-			continue
-
-		var question := QuestionData.new(
-				data["question"],
-				int(data["difficulty"])
+		push_error(
+				"問題文ファイル %s を開けませんでした: %s"
+				% [
+						_path,
+						FileAccess.get_open_error(),
+				]
 		)
+		return
 
-		_questions.append(question)
+	_load_csv(file, true)
+
+
+func _load_csv(csv_file: FileAccess, skip_header: bool) -> void:
+	var length := csv_file.get_length()
+	var line_number := 1
+
+	# ヘッダー行をここで読み込み
+	if skip_header:
+		csv_file.get_csv_line()
+		line_number += 1
+
+	while csv_file.get_position() < length:
+		var elements := csv_file.get_csv_line()
+		if not _is_valid_row(elements, line_number):
+			line_number += 1
+			continue
+
+		_questions.append(
+				QuestionData.new(elements[0].strip_edges(), int(elements[1]))
+		)
+		line_number += 1
+
+
+func _is_valid_row(elements: PackedStringArray, line_number: int) -> bool:
+	if elements.size() != 2:
+		push_warning(
+				"%s %d行目は列数が2ではないため、スキップします" % [_path, line_number]
+		)
+		return false
+
+	if elements[0].strip_edges().is_empty():
+		push_warning(
+				"%s %d行目は問題文が空のため、スキップします" % [_path, line_number]
+		)
+		return false
+
+	if not elements[1].strip_edges().is_valid_int():
+		push_warning(
+				"%s %d行目は難易度が整数ではないため、スキップします" % [_path, line_number]
+		)
+		return false
+
+	return true
 
 
 ## 全部の問題を取得
